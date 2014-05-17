0b605f7dcee8b3c2d063abd042a32f6ba937e560
Ana Paula Vargas <anapaulavnoronha@gmail.com>
2014-05-17 10:17:31 -0300

    Adicionando teste numeros primos ate 2 falhando

---
+++ CrivoDeEratostenes/src/GeradorPrimosTeste.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class GeradorPrimosTeste {
+
+	@Test
+	public void testeGeradorPrimosAteNumeroDois() {
+		
+		GeradorPrimos geradorPrimos = new GeradorPrimos();
+		assertEquals(2, geradorPrimos.gerarPrimosAte(2));
+	
+	}
+
+}
