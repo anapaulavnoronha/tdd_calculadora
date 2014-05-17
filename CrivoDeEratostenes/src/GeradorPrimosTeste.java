32e887ce894f3a4454acb20cc81f76cd3d112bec
Ana Paula Vargas <anapaulavnoronha@gmail.com>
2014-05-17 10:30:11 -0300

    Adicionando padrao setUp

--- CrivoDeEratostenes/src/GeradorPrimosTeste.java
+++ CrivoDeEratostenes/src/GeradorPrimosTeste.java
@@ -1,22 +1,27 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class GeradorPrimosTeste {
 
+	GeradorPrimos geradorPrimos;
+	
+	@Before
+	public void setUp() throws Exception{
+		geradorPrimos = new GeradorPrimos();
+	}
+	
 	@Test
 	public void testeGeradorPrimosAteNumeroDois() {
 		
-		GeradorPrimos geradorPrimos = new GeradorPrimos();
 		assertEquals("2", geradorPrimos.gerarPrimosAte(2));
-	
 	}
 	
 	@Test
 	public void testeGeradorPrimosAteNumeroTres(){
 		
-		GeradorPrimos geradorPrimos = new GeradorPrimos();
 		assertEquals("2,3", geradorPrimos.gerarPrimosAte(3));
 	}
 
