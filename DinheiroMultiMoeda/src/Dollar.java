215d9ed4b90abe8a0fe37a5be21a73e76baacfb3
Ana Paula Vargas <anapaulavnoronha@gmail.com>
2014-05-18 09:55:20 -0300

    Criando a superclasse Money - alterando testeEquality() para testar a igualdade de Franc tambem

--- DinheiroMultiMoeda/src/Dollar.java
+++ DinheiroMultiMoeda/src/Dollar.java
@@ -1,8 +1,7 @@
 
-public class Dollar {
+public class Dollar extends Money{
 
-	private int amount;
-
+	
 	public Dollar(int amount) {
 		this.amount = amount;
 	}
@@ -10,10 +9,6 @@ public class Dollar {
 	public Dollar times(int multiplier) {
 		return new Dollar(amount * multiplier);	
 	}
-	
-	public boolean equals(Object object){
-		Dollar dollar = (Dollar) object;
-		return amount == dollar.amount;
-	}
+
 
 }
