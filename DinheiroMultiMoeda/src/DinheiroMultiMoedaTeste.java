37eedd948ce74716256cd6cf9ec9407980f92d97
Ana Paula Vargas <anapaulavnoronha@gmail.com>
2014-05-18 10:00:37 -0300

    testeEquality - Comparando Franc com Dollar - dois Moneys sao iguais apenas se suas quantidades e suas classes sao iguais

--- DinheiroMultiMoeda/src/DinheiroMultiMoedaTeste.java
+++ DinheiroMultiMoeda/src/DinheiroMultiMoedaTeste.java
@@ -28,6 +28,8 @@ public class DinheiroMultiMoedaTeste {
 		assertFalse(new Dollar(5).equals(new Dollar(6)));
 		assertTrue(new Franc(5).equals(new Franc(5)));
 		assertFalse(new Franc(5).equals(new Franc(6)));
+		
+		assertFalse(new Franc(5).equals(new Dollar(5)));
 	}
 
 }
