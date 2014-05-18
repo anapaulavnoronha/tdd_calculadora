a4813c016cdc5ac59a157c6c340733551ef8c290
Ana Paula Vargas <anapaulavnoronha@gmail.com>
2014-05-18 10:16:47 -0300

    Alterando testes usando METODO FABRICA - Money se torna uma classe abstrata - nenhum codigo cliente sabe que existe as subclasses Dollar e Franc (com codigo por enquanto duplicado)

--- DinheiroMultiMoeda/src/Money.java
+++ DinheiroMultiMoeda/src/Money.java
@@ -1,5 +1,5 @@
 
-public class Money {
+abstract class Money {
 	
 	protected int amount;
 	
@@ -7,5 +7,16 @@ public class Money {
 		Money money = (Money) object;
 		return amount == money.amount && getClass().equals(money.getClass());
 	}
+
+	public static Money dollar(int amount) {
+		return new Dollar(amount);
+	}
+
+	public static Money franc(int amount) {
+		return new Franc(amount);
+	}
+	
+	abstract Money times(int multiplier);
+	
 
 }
